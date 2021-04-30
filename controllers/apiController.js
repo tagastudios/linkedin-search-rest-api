a3c54497ea835e832a74847f77aca714f54e4608
Taga Studios <tagastudios@gmail.com>
2021-04-30 02:32:06 -0400

    Heroku HotFix 3

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -2,11 +2,13 @@ const Course = require("../models/courses");
 
 // GET INDEX AND REDIRECT
 const api_index = (req, res) => {
-  res.redirect("/api/skills");
+  // res.redirect("/api/skills");
+  res.status(404).json({ error: "404 - API Route not found" });
 };
 
 // GET COURSES
 const api_courses = (req, res) => {
+  req.setTimeout(30000);
   Course.find()
     // Sort new to old
     .then((result) => {
@@ -19,47 +21,50 @@ const api_courses = (req, res) => {
 
 // GET ALL UNIQUE SKILLS
 const api_skills = (req, res) => {
+  req.setTimeout(30000);
   Course.find(
     { "classifications.associatedClassification.type": "SKILL" },
     "classifications.associatedClassification.type classifications.associatedClassification.name.value"
   )
-    .then((result) => {
+    .then(async (result) => {
       // res.send(result);
       // Returning only unique Names by SKILLS only!
       let skills = [];
-      try {
-        result.forEach(async (data) => {
-          for (let i = 0; i < result.length; i++) {
-            if (data.classifications[i]) {
-              const skill = await data.classifications[i]
-                .associatedClassification.type;
-              if (skill == "SKILL") {
-                // JOIN
-                const skillName =
-                  data.classifications[i].associatedClassification.name.value;
-                skills.push(skillName);
-              }
+
+      await result.forEach((data) => {
+        for (let i = 0; i < data.classifications.length; i++) {
+          if (data.classifications[i]) {
+            const skill = data.classifications[i].associatedClassification.type;
+            if (skill == "SKILL") {
+              // JOIN
+              const skillName =
+                data.classifications[i].associatedClassification.name.value;
+              skills.push(skillName);
             }
-            if (i == result.length - 1) {
-              let uniqueSkills = [...new Set(skills)];
-              res.json(uniqueSkills);
-              // res.status(200).json({ uniqueSkills });
-            }
+          } else {
+            console.log("test");
+            next(err);
           }
-        });
-      } catch (err) {
-        console.log("Error Skills - 1");
-        res.status(500).json({ err });
-      }
+          if (i == data.classifications.length - 1) {
+            // let uniqueSkills = [...new Set(skills)];
+            // res.json(uniqueSkills);
+            // res.status(200).json({ uniqueSkills });
+          }
+        }
+      });
+
+      let uniqueSkills = [...new Set(skills)];
+      res.json(uniqueSkills);
     })
     .catch((err) => {
       console.log("Error Skills - 2");
-      res.status(500).json({ err });
+      res.json({ err });
     });
 };
 
 // GET COURSES AFTER GIVEN A SKILL
-const api_skill_course = (req, res) => {
+const api_skill_course = (req, res, next) => {
+  req.setTimeout(30000);
   const skill = req.params.id;
   Course.find({
     "classifications.associatedClassification.name.value": {
@@ -68,11 +73,14 @@ const api_skill_course = (req, res) => {
     },
   })
     .then((result) => {
-      res.status(200).json(result);
+      if (result == "") {
+        next(err);
+      }
+      res.json(result);
     })
     .catch((err) => {
-      console.log("Error Skill > Course");
-      res.status(404).send("Skill not found -> " + skill);
+      res.status(404).json({ error: "No data find by " + skill });
+      console.log("Error Courses by Skill - 1");
     });
 };
 
